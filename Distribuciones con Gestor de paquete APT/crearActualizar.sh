1f5818b1168c659e72290b44ab5c9a304a47dc90
Jorge Gutierrez <jorge.gutierrezvila0@gmail.com>
2020-02-24 10:40:19 +0100

    Changes on update script

--- Distribuciones con Gestor de paquete APT/crearActualizar.sh
+++ Distribuciones con Gestor de paquete APT/crearActualizar.sh
@@ -3,8 +3,8 @@
 
 # Script para distribuciones con gestores de paquete APT que crea un fichero con el comando de actualizar y lo pone como herramienta del sistema.
 printf "Introduciendo en el fichero comandos para actualizar el sistema.\n"
-printf "#\!/bin/bash \n sudo apt update -y \n sudo apt upgrade -y \n sudo apt autoremove -y\n" > actualizar.sh #Para uso interactivo se recomienda mas usar apt que apt-get en las versiones mas recientes de ubuntu.
-printf ""
+printf '#!/bin/bash \n sudo apt update -y \n sudo apt upgrade -y \n sudo apt autoremove -y\n' > actualizar.sh #Para uso interactivo se recomienda mas usar apt que apt-get en las versiones mas recientes de ubuntu.
+printf "\n"
 printf "Dando permisos al fichero\n"
 sudo chmod u+x actualizar.sh
 printf "\n"
